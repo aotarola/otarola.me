b0db668a873c2ee8bbb40144ed658772ba228a01
Andres Otarola <andres@otarola.me>
2021-10-10 17:57:37 -0700

    basic styling for index

--- src/Page/Index.elm
+++ src/Page/Index.elm
@@ -1,16 +1,19 @@
 module Page.Index exposing (Data, Model, Msg, page)
 
 import Article
+import Css
 import DataSource exposing (DataSource)
-import DataSource.File
 import Head
 import Head.Seo as Seo
-import Html.Styled exposing (text)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css)
 import Page exposing (Page, StaticPayload)
 import Pages.PageUrl exposing (PageUrl)
 import Pages.Url
 import Route exposing (Route)
 import Shared
+import Tailwind.Breakpoints as Bp
+import Tailwind.Utilities as Tw
 import View exposing (View)
 
 
@@ -67,12 +70,106 @@ type alias Data =
 view :
     Maybe PageUrl
     -> Shared.Model
-    -> StaticPayload Data RouteParams
-    -> View Msg
+    -> StaticPayload Data {}
+    -> View msg
 view maybeUrl sharedModel static =
     { title = "Index page"
     , body =
-        [ text "This is the index page."
-        , text <| "Greeting: "
+        [ div
+            [ css
+                [ Tw.max_w_3xl
+                , Tw.mx_auto
+                , Tw.px_4
+                , Bp.sm [ Tw.px_6 ]
+                , Bp.xl [ Tw.max_w_5xl, Tw.px_0 ]
+                ]
+            ]
+            [ main_ []
+                [ ul
+                    [ css
+                        [ Tw.divide_y
+                        , Tw.divide_gray_200
+                        ]
+                    ]
+                    [ li [ css [ Tw.py_12 ] ]
+                        [ article
+                            [ css
+                                [ Tw.space_y_2
+                                , Bp.xl [ Tw.grid, Tw.grid_cols_4, Tw.space_y_0, Tw.items_baseline ]
+                                ]
+                            ]
+                            [ dl []
+                                [ dt
+                                    [ css
+                                        [ Tw.sr_only
+                                        ]
+                                    ]
+                                    [ text "Published on" ]
+                                , dd
+                                    [ css
+                                        [ Tw.text_base
+                                        , Tw.font_medium
+                                        , Tw.text_gray_500
+                                        ]
+                                    ]
+                                    [ text "August 11, 2021" ]
+                                ]
+                            , div
+                                [ css
+                                    [ Tw.space_y_5
+                                    , Bp.xl
+                                        [ Tw.col_span_3
+                                        ]
+                                    ]
+                                ]
+                                [ div [ css [ Tw.space_y_6 ] ]
+                                    [ h2
+                                        [ css
+                                            [ Tw.text_2xl
+                                            , Tw.font_bold
+                                            , Tw.tracking_tight
+                                            ]
+                                        ]
+                                        [ a
+                                            [ css [ Tw.text_green_500 ]
+                                            ]
+                                            [ text "Introducing Tailwind UI Ecommerce" ]
+                                        ]
+                                    , div
+                                        [ css
+                                            [ Tw.prose
+                                            , Tw.max_w_none
+                                            , Tw.text_gray_500
+                                            ]
+                                        ]
+                                        [ div
+                                            [ css
+                                                [ Tw.prose
+                                                , Tw.max_w_none
+                                                ]
+                                            ]
+                                            [ p [] [ text "Almost 6 months in the making, we finally realeased Tailwind UI Ecommerce -- the first all new component kit for Tailwind UI since the initial launch back in February 2020." ] ]
+                                        ]
+                                    ]
+                                , div
+                                    [ css
+                                        [ Tw.text_base
+                                        , Tw.font_medium
+                                        ]
+                                    ]
+                                    [ a
+                                        [ css
+                                            [ Tw.text_green_500
+                                            , Css.hover [ Tw.text_green_600 ]
+                                            ]
+                                        ]
+                                        [ text "Read more" ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
         ]
     }
