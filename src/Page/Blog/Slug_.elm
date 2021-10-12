3c2fc06a59227a8e9fb8fff8a20d040fa0fcd678
Andres Otarola <andres@otarola.me>
2021-10-12 09:01:31 -0700

    dynamic article

--- src/Page/Blog/Slug_.elm
+++ src/Page/Blog/Slug_.elm
@@ -2,15 +2,18 @@ module Page.Blog.Slug_ exposing (Data, Model, Msg, page)
 
 import Article exposing (ArticleMetadata, frontmatterDecoder)
 import DataSource exposing (DataSource)
+import Date
 import Head
 import Head.Seo as Seo
-import Html.Styled exposing (Html, text)
-import Markdown.Renderer exposing (defaultHtmlRenderer)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes as Attr exposing (css)
 import MarkdownCodec
 import Page exposing (Page, PageWithState, StaticPayload)
 import Pages.PageUrl exposing (PageUrl)
 import Pages.Url
 import Shared
+import Tailwind.Breakpoints as Bp
+import Tailwind.Utilities as Tw
 import TailwindMarkdownRenderer exposing (renderer)
 import View exposing (View)
 
@@ -85,8 +88,107 @@ view :
     -> Shared.Model
     -> StaticPayload Data RouteParams
     -> View Msg
-view maybeUrl sharedModel static =
-    { title = static.data.metadata.title
+view maybeUrl sharedModel staticPayload =
+    { title = staticPayload.data.metadata.title
     , body =
-        static.data.body
+        [ div
+            [ css
+                [ Tw.max_w_3xl
+                , Tw.mx_auto
+                , Tw.px_4
+                , Bp.sm [ Tw.px_6 ]
+                ]
+            ]
+            [ main_ []
+                [ article
+                    [ css
+                        [ Bp.xl
+                            [ Tw.divide_y
+                            , Tw.divide_gray_200
+                            ]
+                        ]
+                    ]
+                    [ headerView staticPayload.data.metadata
+                    , bodyView staticPayload.data.body
+                    ]
+                ]
+            ]
+        ]
     }
+
+
+bodyView : List (Html msg) -> Html msg
+bodyView body =
+    div
+        [ css
+            [ Tw.divide_y
+            , Tw.divide_gray_200
+            ]
+        ]
+        [ div
+            [ css
+                [ Tw.max_w_none
+                , Tw.pt_10
+                , Tw.pb_8
+                ]
+            ]
+            [ div
+                [ css
+                    [ Tw.prose
+                    , Tw.max_w_none
+                    , Tw.text_gray_600
+                    ]
+                ]
+                body
+            ]
+        ]
+
+
+headerView : ArticleMetadata -> Html msg
+headerView metadata =
+    header
+        [ css
+            [ Tw.pt_6
+            , Bp.xl
+                [ Tw.pb_10
+                ]
+            ]
+        ]
+        [ div
+            [ css
+                [ Tw.space_y_1
+                , Tw.text_center
+                ]
+            ]
+            [ dl [ css [ Tw.space_y_10 ] ]
+                [ dd [ css [ Tw.sr_only ] ] []
+                , dd
+                    [ css
+                        [ Tw.text_base
+                        , Tw.font_medium
+                        , Tw.text_gray_500
+                        ]
+                    ]
+                    [ time
+                        [ Attr.datetime "2020-03-16"
+
+                        -- HACK: actually make it dynamic
+                        ]
+                        [ text (metadata.published |> Date.format "MMMM ddd, yyyy") ]
+                    ]
+                ]
+            , div []
+                [ h1
+                    [ css
+                        [ Tw.text_3xl
+                        , Tw.font_extrabold
+                        , Tw.text_gray_900
+                        , Tw.tracking_tight
+                        , Bp.sm [ Tw.text_4xl ]
+                        , Bp.md [ Tw.text_5xl, Tw.leading_3 ] -- FIX: figure out how to do md:leading-[3.5rem]
+                        ]
+                    ]
+                    [ text metadata.title ]
+                ]
+            ]
+        ]
